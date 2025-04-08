878a83bf6821457fdb310df620a64ecbf5a041d3
kb-wonderfi <kartik@wonder.fi>
2025-04-08 08:48:35 -0700

    Adding getters

--- packages/sdk/src/constants.ts
+++ packages/sdk/src/constants.ts
@@ -1,9 +1,13 @@
 export const CONTRACTS: {
     [key: string]: {
         FACTORY: string;
+        FAUCET: string;
+        PAYMASTER: string;
     }
 } = {
     '96371': {
         FACTORY: '0xE96Ce8D9382826Bc8C8aCf76158F7D9AC44ABe57',
+        FAUCET: '0x0d0A98A3f88d6604d6d8B07bB60a79444C42f4bC',
+        PAYMASTER: '0x1cf380DABAFb3a9B2f69017D9d645fBf6D103803',
     },
 };
--- packages/sdk/src/utils.ts
+++ packages/sdk/src/utils.ts
@@ -1,5 +1,5 @@
 import { ContractRunner } from "ethers"
-import { Faucet__factory } from "./contracts"
+import { Factory__factory, Faucet__factory, Paymaster__factory } from "./contracts"
 import { CONTRACTS } from "./constants";
 
 export const getFactory = async (runner: ContractRunner) => {
@@ -12,5 +12,31 @@ export const getFactory = async (runner: ContractRunner) => {
         return null
     }
 
-    return Faucet__factory.connect(contracts.FACTORY, runner);
+    return Factory__factory.connect(contracts.FACTORY, runner);
+}
+
+export const getFaucet = async (runner: ContractRunner) => {
+    if (!runner?.provider) {
+        return null
+    }
+    const network = await runner.provider.getNetwork();
+    const contracts = CONTRACTS[network.chainId.toString()];
+    if (!contracts) {
+        return null
+    }
+
+    return Faucet__factory.connect(contracts.FAUCET, runner);
+}
+
+export const getPaymaster = async (runner: ContractRunner) => {
+    if (!runner?.provider) {
+        return null
+    }
+    const network = await runner.provider.getNetwork();
+    const contracts = CONTRACTS[network.chainId.toString()];
+    if (!contracts) {
+        return null
+    }
+
+    return Paymaster__factory.connect(contracts.PAYMASTER, runner);
 }
